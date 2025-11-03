08cf510f77e103b110f692a2e58a078039b92e3e
Sarah Clark <sarahireneclark@gmail.com>
2025-11-03 03:18:36 -0600

    Initial commit of Xcode project

---
+++ JSONParser/JSONParser/JSONParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParserApp.swift
+//  JSONParser
+//
+//  Created by Sarah Clark on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct JSONParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JSONParser/JSONParserTests/JSONParserTests.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParserTests.swift
+//  JSONParserTests
+//
+//  Created by Sarah Clark on 11/3/25.
+//
+
+import Testing
+@testable import JSONParser
+
+struct JSONParserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
